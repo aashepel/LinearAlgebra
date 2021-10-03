01beebef8196d80460ac9ca2af483e37a9e6f28d
aashepel <aashepel2002@mail.ru>
2021-10-04 00:07:59 +0300

    Initial commit

---
+++ LinearAlgebraLogger/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LinearAlgebraLogger
+{
+    enum LoggerColor
+    {
+        Info = ConsoleColor.Green,
+        Warning = ConsoleColor.Yellow,
+        Error = ConsoleColor.Red,
+        Debug = ConsoleColor.Blue
+    }
+    public static class Logger
+    {
+        public static void LogInfo(string message)
+        {
+            WriteColor("Info: ", message, LoggerColor.Info);
+        }
+        public static void LogWarning(string message)
+        {
+            WriteColor("Warning: ", message, LoggerColor.Warning);
+        }
+        public static void LogError(string message)
+        {
+            WriteColor("Error: ", message, LoggerColor.Error);
+        }
+        public static void LogDebug(string message)
+        {
+            WriteColor("Error: ", message, LoggerColor.Error);
+        }
+        private static void WriteColor(string header, string message, LoggerColor color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(header);
+            Console.ResetColor();
+            Console.WriteLine(new string(' ', 6) + message);
+        }
+    }
+}
---
+++ VectorDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using LinearAlgebra;
+using System;
+using System.Threading.Tasks;
+
+namespace VectorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMathVector vec2 = new MathVector(new double[] { 0, 0, 1, 0 }, nameof(vec2));
+            IMathVector vec1 = new MathVector(vec2, nameof(vec1));
+            IMathVector vec3 = new MathVector(vec1, nameof(vec3));
+            IMathVector vec4 = (MathVector)vec1 + 100;
+            MathVector.PrintLength(vec1);
+            MathVector.PrintLength(vec2);
+            MathVector.PrintLength(vec3);
+            MathVector.Print(vec4);
+        }
+    }
+}
