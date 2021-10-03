e11e6628ed8e22e707849ba0458e94183989fa59
aashepel <aashepel2002@mail.ru>
2021-10-04 00:34:29 +0300

    Added GUI Form for Managing and Testing MathVector

---
+++ LinearAlgebraFormsApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinearAlgebraFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private int _idCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.Rows.Add();
+            var row = dataGridView1.Rows[index];
+            row.Cells[0].Value = ++_idCounter;
+            row.Cells[1].Value = "Vector";
+            
+        }
+    }
+}
